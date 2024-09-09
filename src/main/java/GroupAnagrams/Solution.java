3f5aba7e74d56189503c07eb4fb4f7ae0438545b
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-09 15:34:54 +0330

    Adding doc to group anagrams challenge

--- src/main/java/GroupAnagrams/Solution.java
+++ src/main/java/GroupAnagrams/Solution.java
@@ -4,6 +4,16 @@ import java.util.*;
 
 public class GroupAnagrams {
     public static List<List<String>> solution(List<String> strs) {
+    /**
+     * Solution for 49.GroupAnagrams LeetCode Challenge
+     *
+     * Big O:
+     *  Time complexity: O(n * m * log(m))
+     *  Space complexity: O(n * m)
+     *
+     * @param strs List of strings to check and group the anagrams
+     * @return List of list of string that considered as grouped anagrams
+     */
         Map<String, List<String>> storage = new HashMap<>();
         for (var word : strs) {
             char[] chars = word.toCharArray();
