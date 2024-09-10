bf19764b1153ac99db389e02c8b42ff2ff33d366
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-10 23:52:06 +0330

    Implementing solution for challenge #509

--- src/main/java/FibonacciNumber/Solution.java
+++ src/main/java/FibonacciNumber/Solution.java
@@ -1,4 +1,13 @@
 package FibonacciNumber;
 
 public class Solution {
+    public static int fibonacci(int n) {
+        if (n < 2) return n;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        var result = fibonacci(4);
+        System.out.println(result);
+    }
 }
