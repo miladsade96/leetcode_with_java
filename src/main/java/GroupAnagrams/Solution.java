1fdebd1ed6f610b57e7425eeb5e10a1598d9f1b3
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-10 21:57:28 +0330

    Cleaning the documentation

--- src/main/java/GroupAnagrams/Solution.java
+++ src/main/java/GroupAnagrams/Solution.java
@@ -5,13 +5,12 @@ import java.util.*;
 public class Solution {
     /**
      * Solution for 49.GroupAnagrams LeetCode Challenge
-     *
      * Big O:
      *  Time complexity: O(n * m * log(m))
      *  Space complexity: O(n * m)
      *
      * @param strs List of strings to check and group the anagrams
-     * @return List of list of string that considered as grouped anagrams
+     * @return List of list of strings that considered as grouped anagrams
      */
     public static List<List<String>> groupAnagrams(List<String> strs) {
         Map<String, List<String>> storage = new HashMap<>();
