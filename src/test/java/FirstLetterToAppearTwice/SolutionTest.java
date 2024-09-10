031cba8633c6d4d9dba13113ea12d220c57c8031
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-10 23:36:11 +0330

    Testing the functionality of challenge #2351

--- src/test/java/FirstLetterToAppearTwice/SolutionTest.java
+++ src/test/java/FirstLetterToAppearTwice/SolutionTest.java
@@ -1,4 +1,21 @@
 package FirstLetterToAppearTwice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SolutionTest {
+    @Test
+    void repeatedCharacter() {
+        String input1 = "abcdd";
+        char expected1 = 'd';
+        Assertions.assertEquals(expected1, Solution.repeatedCharacter(input1));
+
+        String input2 = "abccbaacz";
+        char expected2 = 'c';
+        Assertions.assertEquals(expected2, Solution.repeatedCharacter(input2));
+
+        String input3 = "abba";
+        char expected3 = 'b';
+        Assertions.assertEquals(expected3, Solution.repeatedCharacter(input3));
+    }
 }
