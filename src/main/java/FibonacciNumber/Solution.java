d7e3f03138fdb8fa6d32f8d4fd29c87c49832560
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-11 00:30:22 +0330

    Adding the documentation

--- src/main/java/FibonacciNumber/Solution.java
+++ src/main/java/FibonacciNumber/Solution.java
@@ -1,13 +1,17 @@
 package FibonacciNumber;
 
 public class Solution {
+    /**
+     * Solution for 509.Fibonacci Number
+     * Big O:
+     * Time complexity: O(2^n)
+     * Space complexity: O(n)
+     *
+     * @param n fibonacci(n) should be calculated and returned
+     * @return nth fibonacci number
+     */
     public static int fibonacci(int n) {
         if (n < 2) return n;
         return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-
-    public static void main(String[] args) {
-        var result = fibonacci(4);
-        System.out.println(result);
     }
 }
