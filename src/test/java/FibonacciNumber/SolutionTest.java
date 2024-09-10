b13cec2d7b86619c417754fbc4a033b4614c86e8
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-11 00:41:06 +0330

    Testing the functionality of 509.fibonacci number

--- src/test/java/FibonacciNumber/SolutionTest.java
+++ src/test/java/FibonacciNumber/SolutionTest.java
@@ -1,4 +1,25 @@
 package FibonacciNumber;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class SolutionTest {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "1, 1",
+            "2, 1",
+            "3, 2",
+            "4, 3",
+            "5, 5",
+            "6, 8",
+            "7, 13",
+            "8, 21",
+            "9, 34",
+            "10, 55"
+    })
+    void fibonacciTest(int input, int expected) {
+        Assertions.assertEquals(expected, Solution.fibonacci(input));
+    }
 }
