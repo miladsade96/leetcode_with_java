a664d0f170b9043a7a236792ffe222757621a8e7
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-11 22:58:06 +0330

    Implementing missingNumberV1 method

--- src/main/java/MissingNumber/Solution.java
+++ src/main/java/MissingNumber/Solution.java
@@ -1,6 +1,15 @@
 package MissingNumber;
 
+import java.util.Arrays;
+
 public class Solution {
-    public static int missingNumber(int[] nums) {
+    public static int missingNumberV1(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for (var item : nums) {
+            if (count != item) return count;
+            else count++;
+        }
+        return count;
     }
 }
