a8c1e149dcf3c5bbd4a3a21ec84fdc0999fdb92a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-13 21:06:45 +0330

    Adding javadoc for valid anagram solutions including their time and space complexity info

--- src/main/java/ValidAnagram/Solution.java
+++ src/main/java/ValidAnagram/Solution.java
@@ -5,6 +5,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
+    /**
+     * Solution for 242.Valid Anagram - Approach #1
+     * BigO:
+     * Time complexity: O(n log n)
+     * Space complexity: O(n)
+     *
+     * @param s first given string to check
+     * @param t second given string to check
+     * @return true or false, whether s and t are valid anagrams
+     */
     public static boolean isAnagramV1(String s, String t) {
         if (s.length() != t.length()) return false;
         var sChars = s.toCharArray();
@@ -17,6 +27,16 @@ public class Solution {
         return true;
     }
 
+    /**
+     * Solution for 242.Valid Anagram - Approach #2
+     * BigO:
+     * Time complexity: O(n log n)
+     * Space complexity: O(n)
+     *
+     * @param s first given string to check
+     * @param t second given string to check
+     * @return true or false, whether s and t are valid anagrams
+     */
     public static boolean isAnagramV2(String s, String t) {
         if (s.length() != t.length()) return false;
         var sChars = s.toCharArray();
@@ -26,6 +46,16 @@ public class Solution {
         return new String(sChars).equals(new String(tChars));
     }
 
+    /**
+     * Solution for 242.Valid Anagram - Approach #3
+     * BigO:
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     *
+     * @param s first given string to check
+     * @param t second given string to check
+     * @return true or false, whether s and t are valid anagrams
+     */
     public static boolean isAnagramV3(String s, String t) {
         var counts1 = calculateCountsHelper(s);
         var counts2 = calculateCountsHelper(t);
