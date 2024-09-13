014b9ba3af01feb28f4d8c5c92b6974b35a746f5
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-13 17:52:18 +0330

    Testing the functionality of valid anagram methods, v1, v2, v3

--- src/test/java/ValidAnagram/SolutionTest.java
+++ src/test/java/ValidAnagram/SolutionTest.java
@@ -1,4 +1,23 @@
 package ValidAnagram;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class SolutionTest {
+    @ParameterizedTest
+    @CsvSource({
+            "anagram, nagaram, true",
+            "rat, car, false",
+            "angel, glean, true",
+            "arc, car, true",
+            "dusty, study, true",
+            "save, vase, true",
+            "dinn, diin, false"
+    })
+    void validAnagramTest(String s, String t, boolean result) {
+        Assertions.assertEquals(result, Solution.isAnagramV1(s, t));
+        Assertions.assertEquals(result, Solution.isAnagramV2(s, t));
+        Assertions.assertEquals(result, Solution.isAnagramV3(s, t));
+    }
 }
