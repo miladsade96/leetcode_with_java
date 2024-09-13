966925fd79cc5015a7705137ac9791e7dd0c57ee
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-13 21:53:32 +0330

    Implementing the solution for two sum challenge

--- src/main/java/TwoSum/Solution.java
+++ src/main/java/TwoSum/Solution.java
@@ -1,4 +1,29 @@
 package TwoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    /**
+     * Solution for 1.TwoSum challenge
+     * BigO:
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     *
+     * @param nums   Given an array of integers
+     * @param target an integer value  - nums[i] + nums[j] will be target
+     * @return An array of integers - [i, j]
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> data = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (data.containsKey(complement)) {
+                return new int[]{data.get(complement), i};
+            } else {
+                data.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
 }
