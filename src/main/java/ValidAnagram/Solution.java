44f5e622a008bcfa6bcdad796030b43d145ffd8a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-13 17:40:51 +0330

    Implementing isAnagramV1 method

--- src/main/java/ValidAnagram/Solution.java
+++ src/main/java/ValidAnagram/Solution.java
@@ -1,5 +1,21 @@
 package ValidAnagram;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    public static boolean isAnagramV1(String s, String t) {
+        if (s.length() != t.length()) return false;
+        var sChars = s.toCharArray();
+        var tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        for (int i = 0; i < sChars.length; i++) {
+            if (sChars[i] != tChars[i]) return false;
+        }
+        return true;
+    }
+
 
 }
