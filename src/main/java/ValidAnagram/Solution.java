0ea1268206b34e77d0b16f338fd0c99755b1a843
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-09-13 17:41:19 +0330

    Implementing isAnagramV3 method

--- src/main/java/ValidAnagram/Solution.java
+++ src/main/java/ValidAnagram/Solution.java
@@ -26,4 +26,22 @@ public class Solution {
         return new String(sChars).equals(new String(tChars));
     }
 
+    public static boolean isAnagramV3(String s, String t) {
+        var counts1 = calculateCountsHelper(s);
+        var counts2 = calculateCountsHelper(t);
+        return counts1.equals(counts2);
+    }
+
+    private static Map<Character, Integer> calculateCountsHelper(String str) {
+        Map<Character, Integer> counts = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ithChar = str.charAt(i);
+            if (counts.containsKey(ithChar)) {
+                counts.put(ithChar, counts.get(ithChar) + 1);
+            } else {
+                counts.put(ithChar, 1);
+            }
+        }
+        return counts;
+    }
 }
