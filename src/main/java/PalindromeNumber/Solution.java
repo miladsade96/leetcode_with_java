2a65f807ed67c2b2691bbbead5697eb6cebaf7db
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-12-06 22:42:37 +0330

    Implementing the solution for palindrome number challenge

--- src/main/java/PalindromeNumber/Solution.java
+++ src/main/java/PalindromeNumber/Solution.java
@@ -1,4 +1,25 @@
 package PalindromeNumber;
 
 public class Solution {
+    public static boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int reversedNumber = 0;
+        int originalNumber = x;
+
+        while (x > 0) {
+            reversedNumber = reversedNumber * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(1221));
+        System.out.println(isPalindrome(1234321));
+    }
 }
