431a3bdbf8e5c1aa435a874c0d8ea96baf9c416d
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-12-10 13:11:27 +0330

    Drop the non dominants

--- src/main/java/BigO/BigO.java
+++ src/main/java/BigO/BigO.java
@@ -68,6 +68,20 @@ public class BigO {
         }
     }
 
+    /// Drop non dominants:
+    /// BigO(n ^ 2 + n) --> BigO(n ^ 2)
+    public static void printNestedLoopPlusAnotherLoop(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            System.out.println(k);
+        }
+    }
+
     public static void main(String[] args) {
         printNestedLoops(10);
     }
