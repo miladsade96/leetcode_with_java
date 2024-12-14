f7bc6f36cd83f5838517fd6d2ccd8da6b99dc8f3
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-12-14 19:41:27 +0330

    Overview of classes in java

--- src/main/java/ClassesAndPointers/ClassesAndPointers.java
+++ src/main/java/ClassesAndPointers/ClassesAndPointers.java
@@ -1,4 +1,14 @@
 package ClassesAndPointers;
 
 public class ClassesAndPointers {
+    public static void main(String[] args) {
+        Cookie cookieOne = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
+
+        cookieOne.setColor("Red");
+        cookieTwo.setColor("yellow");
+
+        System.out.println(cookieOne.getColor());
+        System.out.println(cookieTwo.getColor());
+    }
 }
---
+++ src/main/java/ClassesAndPointers/Cookie.java
@@ -0,0 +1,17 @@
+package ClassesAndPointers;
+
+public class Cookie {
+    private String color;
+
+    public Cookie(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
