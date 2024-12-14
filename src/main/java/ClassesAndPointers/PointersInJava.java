efc35cdfd18ebe3309febf5b97e1f2b9eb535dd5
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-12-14 19:46:16 +0330

    Not pointers

--- src/main/java/ClassesAndPointers/PointersInJava.java
+++ src/main/java/ClassesAndPointers/PointersInJava.java
@@ -1,4 +1,21 @@
 package ClassesAndPointers;
 
 public class PointersInJava {
+    public static void main(String[] args) {
+        // These are not pointers:
+
+        int num1 = 11;
+        int num2 = num1;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        num1 = 22;
+        System.out.println("-".repeat(50));
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        // -------------------------------------------------------------------------------------------------------------
+    }
 }
